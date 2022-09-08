a45468bd215ee92b9a688de1bf2a064a00c43488
Enrym <97190378+Enrym@users.noreply.github.com>
2022-09-08 22:11:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-let reviews = [];
-reviews[0] = "<i>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, illum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi beatae dignissimos voluptatem? <br><br> -John D.</i>";
-reviews[1]= "<i>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, illum.? <br><br> - Jane D.</i>";
-reviews[2]= "<i>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, illum.? <br><br> - Mark D.</i>";
+// let reviews = [];
+// reviews[0] = "<i>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, illum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi beatae dignissimos voluptatem? <br><br> -John D.</i>";
+// reviews[1]= "<i>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, illum.? <br><br> - Jane D.</i>";
+// reviews[2]= "<i>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, illum.? <br><br> - Mark D.</i>";
 
 
 
-let i = 0;
-function next() {
-    i++; 
-    if (i == 3) {
-        i=0;
-    }
-    document.getElementById("rev").innerHTML = reviews[i];
-}
+// let i = 0;
+// function next() {
+//     i++; 
+//     if (i == 3) {
+//         i=0;
+//     }
+//     document.getElementById("rev").innerHTML = reviews[i];
+// }
 
-function prev() {
-    i--;
-    if (i == -1) {
-        i = 2;
-    }
-    document.getElementById("rev").innerHTML = reviews[i];
-}
+// function prev() {
+//     i--;
+//     if (i == -1) {
+//         i = 2;
+//     }
+//     document.getElementById("rev").innerHTML = reviews[i];
+// }
 
 
 const mobileMenu = document.getElementById("mobileMenu");
